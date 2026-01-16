1e557b0c8f67c3cf834365c2dfa9ab0b0bcce4d3
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2026-01-16 19:38:04 +0530

    sql and three js

---
+++ sql/sample.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE
+--   users (
+--     name text not null primary key,
+--     password not null,
+--     email not null
+--   );
+INSERT users (name, password, email)
+values
+  ('vidu', 'my_password', 'vidu@gmail.com');
